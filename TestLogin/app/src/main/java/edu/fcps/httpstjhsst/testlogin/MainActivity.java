a2b7892b81b168804b81b0b6535e26b5161a63a6
Elise Ong <2019eong@tjhsst.edu>
2018-01-03 01:02:25 -0500

    Multiple user authentification testing successful -- firebase

--- TestLogin/app/src/main/java/edu/fcps/httpstjhsst/testlogin/MainActivity.java
+++ TestLogin/app/src/main/java/edu/fcps/httpstjhsst/testlogin/MainActivity.java
@@ -1,20 +1,32 @@
 package edu.fcps.httpstjhsst.testlogin;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mUsername;
-    private EditText mPassword;
-    private Button mLogin;
-    private TextView mAttempt;
-    private int counter = 5;
+    private EditText mUsername; // username typed in on login screen
+    private EditText mPassword; // password typed in on login screen
+    private Button mLogin;  // login button on login screen
+    private TextView mAttempt;  // TxtView of remaining attempts
+    private int counter = 5;    // Count of remaining attempts
+    private TextView mUserRegister; // Link to Register Screen (for new users)
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;  // shows progress during waiting for authentication
 
 
     @Override
@@ -22,34 +34,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mUsername = (EditText)findViewById(R.id.usernameEditText);
-        mPassword = (EditText)findViewById(R.id.passwordEditText);
-        mLogin = (Button)findViewById(R.id.loginButton);
-        mAttempt = (TextView)findViewById(R.id.attemptTextView);
+        /**** INITIALIZE VARIABLES ****/
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        progressDialog = new ProgressDialog(this);
+
+        mUsername = (EditText)findViewById(R.id.userUsernameET);
+        mPassword = (EditText)findViewById(R.id.userPasswordET);
+        mLogin = (Button)findViewById(R.id.userLoginBTN);
+        mAttempt = (TextView)findViewById(R.id.userAttemptTV);
+        mUserRegister = (TextView)findViewById(R.id.userRegisterTV);
         /******/
         mAttempt.setText("Remaining Attempts: " + String.valueOf(counter));
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(mUsername.getText().toString(), mPassword.getText().toString());
+                String userPseudoEmail = mUsername.getText().toString()+"@gmail.com";  // adds txt to make it a pseudo email address
+                validate(userPseudoEmail, mPassword.getText().toString());
             }
         });
-
-
+        mUserRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 
-    private void validate(String userName, String userPassword){
-        if(userName.equals("Elise") && userPassword.equals("1234")){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);  // starts the activity by switching to it
-        }
-        else{
-            counter--;
-            mAttempt.setText("Remaining Attempts: " + String.valueOf(counter));
+    private void validate(String userPseudoEmail, String userPassword){   // checks if user can login
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
 
-            if(counter == 0){   // AKA no more login attempts
-                mLogin.setEnabled(false);   // disables login button
+        firebaseAuth.signInWithEmailAndPassword(userPseudoEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, SecondActivity.class)); // switch from login screen to homescreen
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    counter--;
+                    mAttempt.setText("Attempts Remaining: " + counter);
+                    progressDialog.dismiss();
+                    if(counter == 0){   // if no more login attempts available
+                        mLogin.setEnabled(false);   // disable login button
+                    }
+                }
             }
-        }
+        });
     }
 }
---
+++ TestLogin/app/src/main/java/edu/fcps/httpstjhsst/testlogin/RegisterActivity.java
@@ -0,0 +1,81 @@
+package edu.fcps.httpstjhsst.testlogin;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mRegisterName, mRegisterUsername, mRegisterPassword;   // Name, username, password of new user on Registration screen
+    private Button mRegister;   // register button on Register screen
+    private TextView mRegisterLogin;    // Link to Login Screen (for returning users)
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        /**** INITIALIZE VARIABLES ****/
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mRegisterName = (EditText)findViewById(R.id.registerNameET);
+        mRegisterUsername = (EditText)findViewById(R.id.registerUsernameET);
+        mRegisterPassword = (EditText)findViewById(R.id.registerPasswordET);
+        mRegister = (Button)findViewById(R.id.registerBTN);
+        mRegisterLogin = (TextView)findViewById(R.id.registerLoginTV);
+        /****/
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate()){
+                    // upload data to database
+                    // database takes "pseudo"-email and password
+                    String newPseudoEmail = mRegisterUsername.getText().toString().trim()+"@gmail.com";
+                    String newPassword = mRegisterPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(newPseudoEmail, newPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));   // redirect back to login page
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        mRegisterLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private Boolean validate(){     // checks if all three fields filled out
+        Boolean result = false;
+        String name = mRegisterName.getText().toString();
+        String username = mRegisterUsername.getText().toString();
+        String password = mRegisterPassword.getText().toString();
+        if(!name.isEmpty() && !username.isEmpty() && !password.isEmpty()){ // if all fields filled out
+            result = true;
+        }
+        return result;
+    }
+}
