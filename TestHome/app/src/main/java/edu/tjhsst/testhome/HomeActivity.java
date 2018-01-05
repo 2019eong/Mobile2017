18d39a2b9916ce9c7aa637d11b2ab7dc80ff14e7
Sreya Mallipeddi <2019smallipe@tjhsst.edu>
2018-01-04 22:34:01 -0500

    testHome shell completed

---
+++ TestHome/app/src/main/java/edu/tjhsst/testhome/HomeActivity.java
@@ -0,0 +1,41 @@
+package edu.tjhsst.testhome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private Button mAddButton;
+    private Button mEditButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mAddButton = (Button) findViewById(R.id.addButton);
+        mEditButton = (Button)findViewById(R.id.editButton);
+
+        mAddButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, AddActivity.class));
+            }
+        });
+
+        mEditButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, EditActivity.class));
+            }
+        });
+
+
+    }
+}
