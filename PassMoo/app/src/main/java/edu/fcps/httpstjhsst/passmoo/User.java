2dc56f10e788bae1e57cbad3d39b2d041ff5f902
Elise Ong <2019eong@tjhsst.edu>
2018-01-10 14:17:19 -0500

    created User class

---
+++ PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/User.java
@@ -0,0 +1,34 @@
+package edu.fcps.httpstjhsst.passmoo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Elise on 1/10/2018.
+ */
+
+public class User {
+    private String name;
+    private String username;
+    private String password;
+    private ArrayList<AccountInfo> userAccounts;
+
+    public User(String n, String u, String p){
+        name = n;
+        username = u;
+        password = p;
+        userAccounts = new ArrayList<AccountInfo>();
+    }
+    public String getName(){
+        return name;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public ArrayList<AccountInfo> getAccountInfo(){
+        return userAccounts;
+    }
+}
