46ed9b41cc8febae08329ed0a93e320088db0d44
Sreya Mallipeddi <2019smallipe@tjhsst.edu>
2018-01-10 13:42:25 -0500

    AddActivity made more aesthetic

--- PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/AddActivity.java
+++ PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/AddActivity.java
@@ -1,13 +1,25 @@
 package edu.fcps.httpstjhsst.passmoo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class AddActivity extends AppCompatActivity {
+
+    private Button mStorePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+
+     /*   mStorePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*/
+
     }
 }
