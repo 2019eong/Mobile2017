3764fa9f38becb650bf0d4b573e92b715c77c966
Sreya Mallipeddi <2019smallipe@tjhsst.edu>
2018-01-22 00:44:18 -0500

    displaying passwords

--- PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/DeleteActivity.java
+++ PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/DeleteActivity.java
@@ -1,11 +1,21 @@
 package edu.fcps.httpstjhsst.passmoo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class DeleteActivity extends AppCompatActivity {
     private EditText mWebsite;
@@ -13,23 +23,88 @@ public class DeleteActivity extends AppCompatActivity {
 
     private Button mDeleteAccount;
 
+    private FirebaseDatabase database;
+    private DatabaseReference currUserRef;
+    private Bundle bundle;
+
+    private Intent homeIntent;
+    private ArrayList<AccountInfo> listAcctString = new ArrayList<AccountInfo>();
+
+    private String mCurrentUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete);
+        database = FirebaseDatabase.getInstance();
+        mCurrentUsername = getIntent().getStringExtra("deleteExtra");  //current user's username
+        currUserRef = database.getReference(mCurrentUsername);
+        homeIntent = new Intent(DeleteActivity.this, HomeActivity.class);
+
+        bundle = new Bundle();
 
         mWebsite = (EditText) findViewById(R.id.websitename);
         mUsername = (EditText) findViewById(R.id.username);
 
-        mDeleteAccount= (Button)findViewById(R.id.deleteButton);
-        mDeleteAccount.setOnClickListener(new View.OnClickListener()
-        {
+        mDeleteAccount = (Button) findViewById(R.id.deleteButton);
+        mDeleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(DeleteActivity.this, "yayy", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(DeleteActivity.this, "yayy", Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(DeleteActivity.this, HomeActivity.class));
+                loadDataAndChangeScreen();
+            }
+        });
 
+    }
+
+    public void loadDataAndChangeScreen() {
+        currUserRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (!dataSnapshot.getValue().equals("N/A")) { // if not the default
+                    listAcctString.add(makeAccountInfo(dataSnapshot.getValue() + ""));
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        currUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Gson gson = new Gson();
+                String jsonAcctStrList = gson.toJson(listAcctString);
+                bundle.putString("accountlist", jsonAcctStrList);
+                bundle.putString("homeExtra", mCurrentUsername);
+                homeIntent.putExtras(bundle);
+                /* startActivity goes WITHIN onDataChange bc you want it to be
+                 * called AFTER all data has been retrieved from Firebase */
+                startActivity(homeIntent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
     }
+    public AccountInfo makeAccountInfo(String acctString) {
+        String[] info = acctString.split(";");
+        return new AccountInfo(info[0], info[1], info[2]);
+    }
+
 }
--- PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/HomeActivity.java
+++ PassMoo/app/src/main/java/edu/fcps/httpstjhsst/passmoo/HomeActivity.java
@@ -1,32 +1,23 @@
 package edu.fcps.httpstjhsst.passmoo;
 
-import android.accounts.Account;
-import android.content.Context;
+
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.Api;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -49,22 +40,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        /**** INITIALIZE VARIABLES ****/
-        database = FirebaseDatabase.getInstance();
-
-        bundle = getIntent().getExtras();
-        mCurrentUsername = bundle.getString("homeExtra");
-        currUserRef = database.getReference(mCurrentUsername);
-        currUserRef.keepSynced(true);
-        /* WORKING -- Takes string representations of each acct, converts each to
-         * AccountInfo object, and adds it to an arraylist of AccountInfo objects. */
-        jsonAcctStrList = bundle.getString("accountlist");
-        Gson gson = new Gson();
-        Type type = new TypeToken<ArrayList<AccountInfo>>(){}.getType();
-        mAccountArray = gson.fromJson(jsonAcctStrList, type);
-        Toast.makeText(HomeActivity.this, mAccountArray.toString(), Toast.LENGTH_LONG).show();
-
         addIntent = new Intent(HomeActivity.this, AddActivity.class);
         deleteIntent = new Intent(HomeActivity.this, DeleteActivity.class);
 
@@ -86,6 +61,53 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(deleteIntent);
             }
         });
+
+        /**** INITIALIZE VARIABLES ****/
+        database = FirebaseDatabase.getInstance();
+
+        bundle = getIntent().getExtras();
+        mCurrentUsername = bundle.getString("homeExtra");
+        currUserRef = database.getReference(mCurrentUsername);
+        currUserRef.keepSynced(true);
+        /* WORKING -- Takes string representations of each acct, converts each to
+         * AccountInfo object, and adds it to an arraylist of AccountInfo objects. */
+        jsonAcctStrList = bundle.getString("accountlist");
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<AccountInfo>>(){}.getType();
+        mAccountArray = gson.fromJson(jsonAcctStrList, type);
+     // Toast.makeText(HomeActivity.this, mAccountArray.toString(), Toast.LENGTH_LONG).show();
+        if(mAccountArray.size()>0) {
+            LinearLayout a = (LinearLayout) findViewById(R.id.ButtonLayout);
+
+            a.setOrientation(LinearLayout.VERTICAL);
+
+            for (int x = 0; x < mAccountArray.size(); x++) {
+               // Toast.makeText(HomeActivity.this, "yes"+x, Toast.LENGTH_LONG).show();
+                Button newButton = new Button(this);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                params.setMargins(0,0,0,20);
+                newButton.setLayoutParams(params);
+                //newButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                newButton.setId(x);
+               // newButton.setPadding(20,0,20,0);
+
+                newButton.setText(mAccountArray.get(x).getWebsite());
+                newButton.setBackgroundColor(Color.parseColor("#FA305E"));
+                newButton.setTextColor(Color.WHITE);
+
+                final int finalX = x;
+                newButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this, "Username: "+mAccountArray.get(finalX).getUsername()+" " +"Password: "+ mAccountArray.get(finalX).getPassword(), Toast.LENGTH_LONG).show();
+                    }
+                });
+                a.addView(newButton);
+
+            }
+        }
+
         /*********************************/
         //need to remember to somehow get new stuff from firebase if coming back to homescreen from add/delete
 
@@ -93,19 +115,9 @@ public class HomeActivity extends AppCompatActivity {
         //use some sort of layout inflater (but idk how)
 
 
-//        if(mAccountInfoArray!=null) {
-//            LinearLayout myRoot = (LinearLayout) findViewById(R.id.LinearLayout01);
-//            LinearLayout a = new LinearLayout(this);
-//            a.setOrientation(LinearLayout.VERTICAL);
-//            for (int x = 0; x < mAccountInfoArray.size(); x++) {
-//                Button btnTag = new Button(this);
-//                btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-//                btnTag.setText(mAccountInfoArray.get(x).getWebsite());
-//                a.addView(btnTag);
-//            }
-//            myRoot.addView(a);
-//            setContentView(a);
-//        }
+
+
+
     }
     public AccountInfo makeAccountInfo(String acctString){
         String[] info = acctString.split(";");
